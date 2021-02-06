abaf008d415aff2d035308fc7907d08d143fd1a1
Anoop Saxena <anupsaxena@gmail.com>
2021-02-06 23:29:17 +0530

    Create HelloController.java

---
+++ app/src/main/java/testApplication/HelloController.java
@@ -0,0 +1,13 @@
+package testApplication;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+    
+    @RequestMapping("/")
+    public String index() {
+        return " Hello guys... from Spring Boot Application!";
+    }   
+}
